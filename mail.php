4618cb499a25f1a2b6487065322045f381249e54
Mariana Montoya <mariana.montoyan@udea.edu.co>
2020-08-01 00:06:02 -0500

    primer despliegue a remoto

---
+++ mail.php
@@ -0,0 +1,13 @@
+<?php
+
+$addressee = 'mariana.montoyan@udea.edu.co';
+$name = $_POST['nombre'];
+$email = $_POST['email'];
+$subject = $_POST['asunto'];
+$message = $_POST['mensaje'];
+
+$letter = "De: $name \n";
+$letter .= "Email: $email \n";
+$letter .= "Mensaje: $message";
+
+mail($addressee, $subject, $letter);
