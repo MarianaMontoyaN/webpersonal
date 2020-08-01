8e2ea18189759a8c00c73990ab7db8fb7403a9cf
Mariana Montoya Naranjo <55904395+MarianaMontoyaN@users.noreply.github.com>
2020-08-01 13:37:18 -0500

    Update mail.php

--- mail.php
+++ mail.php
@@ -12,11 +12,13 @@ $letter = "De: $name \n";
 $letter .= "Email: $email \n";
 $letter .= "Mensaje: $message";
 
+echo "$addressee - $letter";
 
-$from = new SendGrid\Email(null, $email);
-$to = new SendGrid\Email(null, $addressee);
-$content = new SendGrid\Content("text/plain", $letter);
-$mail = new SendGrid\Mail($from, $subject, $to, $content);
+
+$from = new \SendGrid\Email(null, $email);
+$to = new \SendGrid\Email(null, $addressee);
+$content = new \SendGrid\Content("text/plain", $letter);
+$mail = new \SendGrid\Mail($from, $subject, $to, $content);
 
 $apiKey = getenv('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');
 $sg = new \SendGrid($apiKey);
